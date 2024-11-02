4431a7fbc861180858815b924952dd275a7057be
zinh9 <zinh9>
2024-11-02 00:36:52 -0300

    commit de atualizacao 02/11

--- src/main/java/com/projecttattoo/BrenoLendaTattoo/dto/cliente/ClienteDto.java
+++ src/main/java/com/projecttattoo/BrenoLendaTattoo/dto/cliente/ClienteDto.java
@@ -3,5 +3,5 @@ package com.projecttattoo.BrenoLendaTattoo.dto.cliente;
 import java.time.LocalDate;
 
 public record ClienteDto(Integer id, String nome, String sobrenome, String email, String telefone, String cpf, LocalDate dataNascimento) {
-
+    
 }
